3f04906b4243265fe096c04d5b3f64cfdb64af09
chamzasd <chamzas95@gmail.com>
2020-11-01 17:03:04 -0600

    added printing nodes state

--- arm_move/nodes/mover.py
+++ arm_move/nodes/mover.py
@@ -1,1 +1,83 @@
 #!/usr/bin/env python
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+class RobotPX():
+    def __init__(self):
+        super(RobotPX, self).__init__()
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node('RobotPX',anonymous=True,log_level=rospy.DEBUG)
+        robot_name = "px100"
+        self.robot_name = robot_name
+        self.dof = 4
+
+        robot = moveit_commander.RobotCommander()
+        scene = moveit_commander.PlanningSceneInterface()
+        group_name = "interbotix_arm"
+        group = moveit_commander.MoveGroupCommander(group_name)
+        display_trajectory_publisher = rospy.Publisher("move_group/display_planned_path",
+                                                    moveit_msgs.msg.DisplayTrajectory,
+                                                    queue_size=20)
+
+        ## Getting Basic Information
+        planning_frame = group.get_planning_frame()
+        print( "============ Reference frame: %s" % planning_frame)
+
+        eef_link = group.get_end_effector_link()
+        print( "============ End effector: %s" % eef_link)
+
+        group_names = robot.get_group_names()
+        print( "============ Robot Groups:", robot.get_group_names())
+
+        print( "============ print(ing robot state")
+        print( robot.get_current_state())
+        print( "")
+
+
+        # Misc variables
+        self.box_name = ''
+        self.robot = robot
+        self.scene = scene
+        self.group = group
+        self.display_trajectory_publisher = display_trajectory_publisher
+        self.planning_frame = planning_frame
+        self.eef_link = eef_link
+        self.group_names = group_names
+
+
+
+    def update(self):
+        rate = rospy.Rate(2) # publish freacuancy (DO NOT Change)
+        while not rospy.is_shutdown():
+            rospy.logdebug("Hello!")
+            self.print_joint_state()
+            rate.sleep()
+
+    def get_joint_state(self):
+        return self.group.get_current_joint_values()
+    
+    def print_joint_state(self):
+        joint_state = self.group.get_current_joint_values()
+        i=0
+        for joint in joint_state: 
+            rospy.logdebug("joint "+str(i)+ ": " +str(joint*180/pi))
+            i+=1
+        rospy.logdebug("------")
+
+
+
+
+if __name__ == '__main__':
+    try:
+        robot = RobotPX()
+        robot.update()
+    except rospy.ROSInterruptException:
+        pass
